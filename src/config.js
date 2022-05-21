cfe0c266c703e85f72f6547edd936929ed92fefa
yopaseopor <yopaseopor@gmail.com>
2022-05-21 23:29:09 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -638,6 +638,38 @@ var config = {
 				});
 				return style;
 			}
+  },
+		{
+			group: 'Test',
+			title: '0-99',
+			query: '(nwr[~"^check_date$"~"20[0-9][0-9]."]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'base/circle.svg',
+			iconStyle: 'background-color:#753f4f',
+			style: function (feature) {
+				var key_regex = /^check_date$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(0,0,255,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: '#753f4f',
+					width: 5
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Circle({
+						fill: fill,
+						stroke: stroke,
+						radius: 5
+					}),
+							text: new ol.style.Text({
+								text: name
+							}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
 		},
 		{
 			group: 'Test',
