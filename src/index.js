969937217f8206646f58755279c9ac1b57f47aee
yopaseopor <yopaseopor@gmail.com>
2022-05-21 12:31:16 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -332,7 +332,7 @@ $(function () {
 	// Info Control
 	var infoControlBuild = function () {
 		var container = $('<div>').addClass('ol-control ol-unselectable osmcat-infobutton').html($('<button type="button"><i class="fa fa-info-circle"></i></button>').on('click', function () {
-			window.location.href = 'https://github.com/yopaseopor/osmhistoricmap';
+			window.location.href = 'https://github.com/yopaseopor/osmvalidatormap';
 		}));
 		return container[0];
 	};
